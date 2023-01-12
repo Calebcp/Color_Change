0c2810fc5187fa5008f92bb5119449e76f131d6a
Calebcp <107895663+Calebcp@users.noreply.github.com>
2023-01-12 18:24:42 -0500

    Add files via upload

---
+++ Color_Change/color_change.js
@@ -0,0 +1,63 @@
+
+function ch_Color1(color) {
+    document.body.style.backgroundColor = color;
+    showAlert1();
+ }
+
+ function ch_Color2(color) {
+    document.body.style.backgroundColor = color;
+    showAlert2();
+ }
+
+ function ch_Color3(color) {
+    document.body.style.backgroundColor = color;
+    showAlert3();
+ }
+
+ function ch_Color4(color) {
+    document.body.style.backgroundColor = color;
+    showAlert4();
+ }
+
+ function ch_Color5(color) {
+    document.body.style.backgroundColor = color;
+    showAlert5();
+ }
+
+ function ch_Color6(color) {
+    document.body.style.backgroundColor = color;
+    showAlert6();
+ }
+
+ function ch_Color7(color) {
+    document.body.style.backgroundColor = color;
+    showAlert7();
+ }
+
+ function showAlert1(){
+    alert("Background Color Changing to Red!");
+ }
+
+ function showAlert2(){
+    alert("Background Color Changing to Orange!");
+ }
+
+ function showAlert3(){
+    alert("Background Color Changing to Yellow!");
+ }
+
+ function showAlert4(){
+    alert("Background Color Changing to Green!");
+ }
+
+ function showAlert5(){
+    alert("Background Color Changing to Blue!");
+ }
+
+ function showAlert6(){
+    alert("Background Color Changing to Indigo!");
+ }
+
+ function showAlert7(){
+    alert("Background Color Changing to Violet!");
+ }
